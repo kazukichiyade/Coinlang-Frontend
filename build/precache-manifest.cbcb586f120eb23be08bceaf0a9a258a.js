c8a7829052fb7c167aa3e78035ee08ad037ed90a
kazukichi <zexyl_x@icloud.com>
2020-02-14 02:04:30 +0900

    add build

---
+++ build/precache-manifest.cbcb586f120eb23be08bceaf0a9a258a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ad6c00ad24b5e59266f632e3ba0011",
+    "url": "/index.html"
+  },
+  {
+    "revision": "538c6bb7ef743a89795d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "69eac820fc41295a6866",
+    "url": "/static/js/2.e085b0bf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e085b0bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538c6bb7ef743a89795d",
+    "url": "/static/js/main.7f10e7df.chunk.js"
+  },
+  {
+    "revision": "e2a61f201a4875820ec0",
+    "url": "/static/js/runtime-main.c25c560a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
