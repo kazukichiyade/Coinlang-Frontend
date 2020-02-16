61b6ea5a5f63b48e89669621f5655f2827a9542b
kazukichi <zexyl_x@icloud.com>
2020-02-16 23:45:29 +0900

    yarn add material-ui, styled-components

--- src/App.js
+++ src/App.js
@@ -1,56 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Link, Redirect, withRouter } from 'react-router-dom';
 import Home from '../src/component/PageHome';
+import Login from '../src/component/PageLogin';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div>
         <div>
-          <Link to="/">HOME</Link>
+          <Link to="/">Coinlang</Link>
+          <Link to="/login">LOGIN</Link>
         </div>
         <Route exact path="/" render={() => <Home />} />
+        <Route exact path="/login" render={() => <Login />} />
       </div>
     </BrowserRouter>
   );
 };
 
 export default App;
-
-// import React, { useState, useEffect } from 'react';
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter, Route, Switch } from 'react-router-dom';
-// import Index from '../src/component';
-
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <Switch>
-//       <Route path="/" component={Index} />
-//     </Switch>
-//   </BrowserRouter>,
-//   document.getElementById('root')
-// );
-
-// import React, { useState, useEffect } from 'react';
-
-// const App = () => {
-//   return (
-//     <body>
-//       <header>
-//         <div class="layout-header">
-//           <a class="main-logo" href="/">
-//             Coinlang
-//           </a>
-//           <a class="signup-button" href="/signup">
-//             <p class="signup">SIGNUP</p>
-//           </a>
-//           <a class="login-button" href="/login">
-//             <p class="login">LOGIN</p>
-//           </a>
-//         </div>
-//       </header>
-//     </body>
-//   );
-// };
-
-// export default App;
