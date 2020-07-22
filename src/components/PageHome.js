32db7c7d34b6018075fc93aa6684e4941d972a97
kazukichi <zexyl_x@icloud.com>
2020-07-22 11:54:01 +0900

    material uiによるデザイン修正途中

--- src/components/PageHome.js
+++ src/components/PageHome.js
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { Button } from '@material-ui/core';
-
 import CoinType from '../components/CoinType';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
 
 const StyledButton = styled(Button)`
   background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
@@ -16,10 +20,15 @@ const StyledButton = styled(Button)`
   box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
 `;
 
+const CARD = styled(Card)`
+  height: 300px;
+`;
+
 const Coin = styled.div`
   margin-top: 60px;
   margin-left: 30px;
-  max-width: 185px;
+  max-width: 220px;
+  height: 500px;
 `;
 
 const Name = styled.div`
@@ -74,24 +83,42 @@ const Home = () => {
       </StyledButton> */}
       {/* Coin名表示 */}
       <Coin>
-        <Name>
-          <CoinType coin={bitCoin} />
-        </Name>
-        <Row>
-          <BID>
-            <span>BID(売)</span>
-            <span>{bitCoin.best_bid}</span>
-          </BID>
-          <LTP>
-            <span>LTP</span>
-            <span>{bitCoin.ltp}</span>
-          </LTP>
-          <ASK>
-            <span>ASK(買)</span>
-            <span>{bitCoin.best_ask}</span>
-          </ASK>
-        </Row>
-
+        <CARD>
+          <CardActionArea>
+            {/* <CardMedia
+              className={classes.media}
+              image="/static/images/cards/contemplative-reptile.jpg"
+              title="Contemplative Reptile"
+            /> */}
+            <CardContent>
+              <Name>
+                <CoinType coin={bitCoin} />
+              </Name>
+              <Row>
+                <BID>
+                  <span>BID(売)</span>
+                  <span>{bitCoin.best_bid}</span>
+                </BID>
+                <LTP>
+                  <span>LTP</span>
+                  <span>{bitCoin.ltp}</span>
+                </LTP>
+                <ASK>
+                  <span>ASK(買)</span>
+                  <span>{bitCoin.best_ask}</span>
+                </ASK>
+              </Row>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </CARD>
         {/* <span> 時間 {bitCoin.timestamp}</span> */}
       </Coin>
     </div>
